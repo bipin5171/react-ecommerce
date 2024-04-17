e066902c5413ed51fa5950123fba378d82b6291e
bipin5171 <bipinbhardwaj818@gmail.com>
2024-04-17 18:41:36 +0530

    part6 - Component division

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -64,7 +64,7 @@ export default function Cart() {
                     <div>
                       <div className="flex justify-between text-base font-medium text-gray-900">
                         <h3>
-                          <a href={product.href}>{product.name}</a>
+                          <div href={product.href}>{product.name}</div>
                         </h3>
                         <p className="ml-4">{product.price}</p>
                       </div>
