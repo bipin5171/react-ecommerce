639889fb7c07979d3d87b4cd4a842bd418b5ac93
bipin5171 <bipinbhardwaj818@gmail.com>
2024-04-19 11:42:36 +0530

    Added brand and categories API.

--- src/features/product/ProductAPI.js
+++ src/features/product/ProductAPI.js
@@ -1,27 +1,56 @@
-// A mock function to mimic making an async request for data
-//Todo: we will note hard-code server URL here
 export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-   const response = await fetch('http://localhost:8080/products')
-   const data = await response.json()
-   resolve({data})
-  }
-  );
+    //TODO: we will not hard-code server URL here
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
 }
 
-export function fetchProductsByFilters(filter) {
-  //filter = {"category":"smartphone"}
-  //TODO : on server we will support multiple values 
+export function fetchProductsByFilters(filter, sort, pagination) {
+  // filter = {"category":["smartphone","laptops"]}
+  // sort = {_sort:"price",_order="desc"}
+  // pagination = {_page:1,_limit=10}
+  // TODO : on server we will support multi values in filter
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+  console.log(pagination);
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
+  }
 
-  let queryString = '';
-  for(let key in filter){
-    queryString += `${key}=${filter[key]}&`
-    console.log(queryString)
-  }
   return new Promise(async (resolve) => {
-   const response = await fetch('http://localhost:8080/products?'+ queryString )
-   const data = await response.json()
-   resolve({data})
-  }
-  );
+    //TODO: we will not hard-code server URL here
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    const totalItems = await response.headers.get("X-Total-Count");
+    resolve({ data: { products: data, totalItems: +totalItems } });
+  });
+}
+
+export function fetchCategories() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/categories");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchBrands() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/brands");
+    const data = await response.json();
+    resolve({ data });
+  });
 }
